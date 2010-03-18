a04d13fa12d0d17b86783a6d3fb18a256a5aebf6
ben.he.src <ben.he.src@f3485be4-0bd3-11df-ad0c-0fb4090ca1bc>
2010-03-18 03:03:24 +0000

    Initial commit of src_optimisation_tr3

---
+++ trunk/src_optimisation_tr3/uk/ac/gla/terrier/optimisation/OneDimensionOptimiser.java
@@ -0,0 +1,19 @@
+/*
+ * Created on 16-Jun-2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package uk.ac.gla.terrier.optimisation;
+
+/**
+ * @author Vassilis Plachouras
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface OneDimensionOptimiser {
+	public double optimise();
+	
+	public double getxmin();
+}
